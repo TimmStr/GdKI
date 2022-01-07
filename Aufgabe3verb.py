20436795b742a963da9a56e5328ed1ad419879eb
Achilleus-hub <83076737+Achilleus-hub@users.noreply.github.com>
2022-01-07 12:55:35 +0100

    Seasonal Plot implemnentiert, fehlt noch Perzentil

--- Aufgabe3verb.py
+++ Aufgabe3verb.py
@@ -8,15 +8,10 @@ dateList = df['yyyyddd']
 def columnPicker():
     columnCounter = 0
 
-    colorBlue = [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
-    colorRed = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1,1 ]
-    alpha=[1,0.8,0.6,0.4,0.2,0.1,0.2,0.4,0.6,0.8,1]
-
     # For Schleife für Spalten aus DF
     for column in df:
         minList = []
         maxList = []
-        secondPlot = []
 
         # >=1 da die Datum Spalte übersprungen wird und kleiner 18 da es nur 17 Spalten gibt
         if (columnCounter >= 1 and columnCounter < 18):
@@ -39,15 +34,14 @@ def columnPicker():
                     dateCompare = str(dateList[z])
                     # umformatieren in Format zum Vergleich. Bei nr 019 kam auch 2019 anstatt Tag 019
                     dateEquation = dateCompare[4:]
+                    yearEquation = dateCompare[:4]
 
                     # Abfrage ob bearbeiteter Wert aus For Schleife in Einträgen der Spalte Datum ist
                     if (date in dateEquation):
-                        # if any(str(j) in s for s in x):
                         if min > actColumn[z]:
                             min = actColumn[z]
                         if max < actColumn[z]:
                             max = actColumn[z]
-                        secondPlot.append(actColumn[z])
                 minList.append(min)
                 maxList.append(max)
             plt.xlabel('Tage')
@@ -56,16 +50,6 @@ def columnPicker():
             plt.plot(minList, color='green', label='Minimum')
             plt.legend()
             plt.show()
-
-            #Farbverlauf klappt nur macht immer ein Plot komplett einfarbig
-            #plt.plot(secondPlot, color=(colorBlue[columnCounter-1],0,colorRed[columnCounter-1],alpha[columnCounter-1]))
-            plt.plot(secondPlot)
-
-            plt.show()
-
-            ##seasonal Plot
-
-
 
 
         elif (columnCounter >= 18):
@@ -78,11 +62,48 @@ def columnPicker():
 columnPicker()
 
 
+
+
+
 def plotSeasonal():
-    for j in range(1, 365):
-        if (j < 10):
-            date = '00' + str(j)
-        elif (j < 100):
-            date = '0' + str(j)
-        else:
-            date = str(j)
+    colorBlue = [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0,0,0]
+    colorRed = [0, 0, 0, 0, 0, 0,0,0, 1, 1, 1, 1, 1, 1,1,1]
+    colorAlpha = [1,0.9, 0.8, 0.6, 0.4,0.3, 0.2, 0.1, 0.2,0.3, 0.4, 0.6, 0.8, 0.9, 1,1]
+    columnCounter = 0
+    # For Schleife für Spalten aus DF
+    for column in df:
+        # >=1 da die Datum Spalte übersprungen wird und kleiner 18 da es nur 17 Spalten gibt
+        if (columnCounter >= 1 and columnCounter < 18):
+            print('Durchgang' + str(columnCounter))
+            actColumn = df[column]
+            colorCounter=0
+
+            # bringen der Zahlen ins richtige Zahlenformat damit Vergleich ermöglicht wird
+            for j in range(2006, 2021):
+                secondPlot = []
+                colorCounter=colorCounter+1
+
+                # fürs ablaufen aller Einträge in actColumn
+                for z in range(0, len(actColumn)):
+                    dateCompare = str(dateList[z])
+                    # umformatieren in Format zum Vergleich. Jahr
+                    yearEquation = dateCompare[:4]
+
+                    # Abfrage ob bearbeiteter Wert aus For Schleife in Einträgen der Spalte Datum ist
+                    if (str(j) in yearEquation):
+                        secondPlot.append(actColumn[z])
+                plt.title('Column: '+str(columnCounter))
+                plt.xlabel('Tage')
+                plt.ylabel('SQKM')
+                print(colorBlue[colorCounter])
+                plt.plot(secondPlot, label=str(j),
+                         color=(colorBlue[colorCounter], 0, colorRed[colorCounter], colorAlpha[colorCounter]))
+
+            plt.legend()
+            plt.show()
+        elif (columnCounter >= 18):
+            break;
+        columnCounter = columnCounter + 1
+
+
+plotSeasonal()
