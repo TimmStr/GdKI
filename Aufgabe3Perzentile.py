eef962a25931726b1b0560af0e2ad86907a55cf0
TimmStr <timm.straub@gmx.de>
2022-02-06 19:35:53 +0100

    Aufgabe 3 und 4 kommentiert

--- Aufgabe3Perzentile.py
+++ Aufgabe3Perzentile.py
@@ -1,32 +1,41 @@
 from matplotlib import pyplot as plt
 import pandas as pd
-from LoadData import pullMasie
 import numpy as np
 
-#pullMasie()
+# read csv-data in dataframe
 df = pd.read_csv('masie_4km_allyears_extent_sqkm.csv', header=1, delimiter=',')
 
+
+# get column name that represents specific region
 def getName(name):
     liste = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     while (name[0] == ' ' or name[0] == '(' or name[0] == ')' or name[0] in liste):
         name = name[1:]
     return name
 
+
+# calculate specific percentil
 def calcPerzent(liste, perzentil):
-    liste=pd.DataFrame(liste)
-    wert=liste.quantile(q=perzentil/100)
+    liste = pd.DataFrame(liste)
+    wert = liste.quantile(q=perzentil / 100)
     return wert
 
+
+# calculate minimum of list
 def calcMin(liste):
-    liste=pd.DataFrame(liste)
-    wert=liste.min()
+    liste = pd.DataFrame(liste)
+    wert = liste.min()
     return wert
 
+
+# calculate maximum of list
 def calcMax(liste):
-    liste=pd.DataFrame(liste)
-    wert=liste.max()
+    liste = pd.DataFrame(liste)
+    wert = liste.max()
     return wert
 
+
+# arrange values in lists to perform min, max, quantiles
 def minMax(column, dateList, columnCounter):
     minList = []
     maxList = []
@@ -35,7 +44,7 @@ def minMax(column, dateList, columnCounter):
     perzent50 = []
     perzent75 = []
 
-    # bringen der Zahlen ins richtige Zahlenformat damit Vergleich ermöglicht wird
+    # arrange numbers to required format to compare them
     for j in range(1, 365):
         liste_tageswerte = []
         if (j < 10):
@@ -44,79 +53,88 @@ def minMax(column, dateList, columnCounter):
             date = '0' + str(j)
         else:
             date = str(j)
-        # fürs ablaufen aller Einträge in actColumn
+        # iterate over values in actual column
         for z in range(0, len(column)):
             dateCompare = str(dateList[z])
-            # umformatieren in Format zum Vergleich. Bei nr 019 kam auch 2019 anstatt Tag 019
+            # convert date value in specific format to compare actual year
             dateEquation = dateCompare[4:]
-            # Abfrage ob bearbeiteter Wert aus For Schleife in Einträgen der Spalte Datum ist
+            # compare actual date value with entries in date-list
             if (date in dateEquation):
                 liste_tageswerte.append(column[z])
 
+        # calculate min/max and append value to related list
         minList.append(calcMin(liste_tageswerte))
         maxList.append(calcMax(liste_tageswerte))
-        # Perzent hat alle Objekte zur aktuellen Spalte mit dem Datum xy
-        # Aufruf Methode zum Perzentil berechnen
+
+        # calculate 25, 50, 75 % percentile and append to related list
         perzent25.append(calcPerzent(liste_tageswerte, 25))
         perzent50.append(calcPerzent(liste_tageswerte, 50))
         perzent75.append(calcPerzent(liste_tageswerte, 75))
 
+    # build plot
     plt.xlabel('Monat')
-    plt.ylabel('SQKM')
-    plt.title('Min Max Function   Column: ' + str(columnCounter))
+    plt.ylabel('Ice Extent in [sqkm]')
+    plt.title('Min Max Plot Region: ' + str(columnCounter))
     plt.xticks(np.arange(0, 365, 30.5),
-    ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'])
+               ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'])
+    # add min/max and percentile lists to plot
     plt.plot(maxList, color='blue', label='Maximum')
     plt.plot(perzent75, color='cyan', label='Perzentil 75')
     plt.plot(perzent50, color='yellow', label='Perzentil 50')
     plt.plot(perzent25, color='red', label='Perzentil 25')
     plt.plot(minList, color='green', label='Minimum')
-    counter=0
+    counter = 0
     for i in minList:
-        counter=counter+1
+        counter = counter + 1
     plt.legend()
     plt.show()
 
 
+# function for plotting seasonal plot
 def plotSeasonal(column, dateList, columnCounter):
-    colorBlue = [1, 1, 1, 1, 1, 1, 1, 1, 0,0, 0, 0, 0, 0, 0, 0, 0]
-    colorRed = [0, 0, 0, 0, 0, 0, 0, 0, 1,1, 1, 1, 1, 1, 1, 1, 1]
-    colorAlpha = [1, 0.875, 0.75, 0.625, 0.5, 0.375, 0.25, 0.125, 0.05,0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1]
-    # For Schleife für Spalten aus DF
+    # define color gradient
+    colorBlue = [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    colorRed = [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    colorAlpha = [1, 0.875, 0.75, 0.625, 0.5, 0.375, 0.25, 0.125, 0.05, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1]
 
     colorCounter = 0
 
-    # bringen der Zahlen ins richtige Zahlenformat damit Vergleich ermöglicht wird
+    # arrange numbers to required format to compare them
     for j in range(2006, 2022):
-        secondPlot = []
+        seasonal_plot = []
         colorCounter = colorCounter + 1
 
-        # fürs ablaufen aller Einträge in actColumn
+        # iterate over values in actual column
         for z in range(0, len(column)):
             dateCompare = str(dateList[z])
-            # umformatieren in Format zum Vergleich. Jahr
+            # convert date value in specific format to compare actual year
             yearEquation = dateCompare[:4]
 
-            # Abfrage ob bearbeiteter Wert aus For Schleife in Einträgen der Spalte Datum ist
+            # compare date value as string with entries in year-list
             if (str(j) in yearEquation):
-                secondPlot.append(column[z])
+                seasonal_plot.append(column[z])
 
-        plt.title('Seasonal Function   Column: ' + str(columnCounter))
+        # build plot
+        plt.title('Seasonal Plot Region: ' + str(columnCounter))
         plt.xlabel('Monat')
-        plt.ylabel('SQKM')
-        plt.xticks(np.arange(0, 365, 31.0),['Jan','Feb','Mär','Apr','Mai','Jun','Jul','Aug','Sep','Okt','Nov','Dez'])
-        plt.plot(secondPlot, label=str(j),
-                 color=(colorBlue[colorCounter], 0, colorRed[colorCounter], colorAlpha[colorCounter]))
+        plt.ylabel('Ice Extent in [sqkm]')
+        plt.xticks(np.arange(0, 365, 31.0),
+                   ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'])
+        plt.plot(seasonal_plot, label=str(j),
+                 color=(colorRed[colorCounter], 0, colorBlue[colorCounter], colorAlpha[colorCounter]))
         ##ging nicht mit cmap = seismic
 
-    plt.legend()
+    # show legend on right side of figure
+    plt.legend(bbox_to_anchor=(1.15, 1.0))
     plt.show()
 
 
+# start function: calls other functions and runs whole program
 def start(df):
     dateList = df['yyyyddd']
+    # iterate over all columns; except date-column
     for column in df:
-        if column!='yyyyddd':
+        if column != 'yyyyddd':
             name = getName(column)
             print('Durchgang: ' + str(name))
             actColumn = df[column]
@@ -124,5 +142,5 @@ def start(df):
             plotSeasonal(actColumn, dateList, name)
 
 
-
+# call start-function
 start(df)
--- Aufgabe4.py
+++ Aufgabe4.py
@@ -1,27 +1,33 @@
-# Quellen
-# https://machinelearningmastery.com/remove-trends-seasonality-difference-transform-python/
 import pandas as pd
 from statsmodels.graphics.tsaplots import plot_acf
 from matplotlib import pyplot as plt
 
+# read csv-data in dataframe
 df = pd.read_csv('masie_4km_allyears_extent_sqkm.csv', header=1, delimiter=',')
 
-list_names=[]
-list_mean_interv1=[]
-list_mean_interv2=[]
-list_mean_interv3=[]
-list_var_interv1=[]
-list_var_interv2=[]
-list_var_interv3=[]
+# define lists for names and intervals 1-3
+list_names = []
+
+# 3 intervals for mean
+list_mean_interv1 = []
+list_mean_interv2 = []
+list_mean_interv3 = []
+
+# 3 intervals for variance
+list_var_interv1 = []
+list_var_interv2 = []
+list_var_interv3 = []
 
 
-
+# calculate mean
 def calc_mean(column, start, end):
     return column.iloc[start:end + 1].mean()
 
+# calculate variance
 def calc_var(column, start, end):
     return column.iloc[start:end+1].var()
 
+# function calls calc_mean and calc_var and appends values to specific interval-lists
 def calc_intervall(column,intervall=5):
     start = 0
     end = start + intervall * 365
@@ -38,32 +44,38 @@ def calc_intervall(column,intervall=5):
         start = end + 1
         end = end + intervall * 365
 
+# plots acf-plots and call calc_intervall
 def calc(df,lags=365,intervall=5):
     for column in df.iloc[:, 1:]:
-        # calc_mean_var_for_single_years(df, column)
+        # plot acf-plot
         plot_acf(df[column], lags=lags)
         plt.title(column)
         plt.show()
+
+        # call calc_intervall to perform mean/var caluclations
         calc_intervall(column,intervall)
+
+# get columns names and append to list_names
 def names(df):
     for column in df:
         if column!='yyyyddd':
             list_names.append(column)
 
+# start function: calls other functions and runs whole program
 def start():
     calc(df)
     names(df)
     i = 0
     for column in df:
-        if i ==0:
+        if i == 0:
             print()
-            i=i+1
+            i = i + 1
         else:
-            print('Mittelwert für Region: '+list_names[i-1])
+            print('Mittelwert für Region: '+ list_names[i-1])
             print(list_mean_interv1[i-1])
             print(list_mean_interv2[i-1])
             print(list_mean_interv3[i-1])
-            print('Varianz für Region: '+list_names[i-1])
+            print('Varianz für Region: '+ list_names[i-1])
             print(list_var_interv1[i-1])
             print(list_var_interv2[i-1])
             print(list_var_interv3[i-1])
@@ -71,6 +83,5 @@ def start():
             print()
             i = i + 1
 
-    df_diff = df.diff().dropna()
-    calc(df_diff)
+# call start-function and run whole program
 start()
